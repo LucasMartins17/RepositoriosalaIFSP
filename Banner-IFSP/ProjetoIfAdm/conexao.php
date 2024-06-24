8c474a53cb9022199e452ecf7ebc66594d694e92
natan-mendess <150560847+natan-mendess@users.noreply.github.com>
2024-06-24 17:43:04 -0300

    Delete Banner-IFSP/ProjetoIfAdm directory

--- Banner-IFSP/ProjetoIfAdm/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "geral");
-    $cod = mysqli_set_charset($conn, 'utf8');
-
-    if(!$conn){
-        die("Conexao falhou: " . mysqli_connect_error());
-    }
